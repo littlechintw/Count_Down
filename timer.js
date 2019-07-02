fece78198611c1517d679d45f9a2f3702af64261
littlechin <littlechintw@gmail.com>
2019-07-03 00:26:12 +0800

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,40 @@
+function getRTime(){
+    urlinfo=window.location.href;
+    len=urlinfo.length;
+    offset=urlinfo.indexOf("?");
+    newsidinfo=urlinfo.substr(offset,len)
+    newsids=newsidinfo.split("=");
+    newsid=newsids[1];
+	var EndTime= new Date(newsid.replace("%20", " "));
+	
+	document.getElementById("text").innerHTML = '倒數至 ' + String(EndTime);
+	var NowTime = new Date();
+	var t =EndTime.getTime() - NowTime.getTime();
+
+	var d=Math.floor(t/1000/60/60/24);
+	var h=Math.floor(t/1000/60/60%24);
+	var m=Math.floor(t/1000/60%60);
+	var s=Math.floor(t/1000%60);
+	
+	var timetext='';
+    timetext += String(h) + ' : ';
+    if(m<10){
+	    timetext += '0' + String(m);
+	}
+	else{
+	    timetext += String(m);
+	}
+	timetext += ' : ';
+	if(s<10){
+	    timetext += '0' + String(s);
+	}
+	else{
+	    timetext += String(s);
+	}
+	
+	if(t<0){
+	    timetext = 'Time\'s UP!!';
+	}
+	document.getElementById("time").innerHTML = timetext;
+}
+setInterval(getRTime,200);
